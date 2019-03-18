71254e2b0f122f2d047f18be4056019591c8145e
Allen Raymond Dumaua <dumauar@gmail.com>
2019-03-17 22:26:11 -0700

    Create Main.php

---
+++ Main.php
@@ -0,0 +1,10 @@
+<html>
+    <head>     
+<link rel = "stylesheet" href = "MainMenu.css" type = "text/css"/>
+</head>
+        <body>
+        <?php
+include("MainMenu.php");
+        ?>
+        </body>
+        </html>
