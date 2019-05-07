1cd10bc88c6325b5a7ab310416775109822f19dd
xxdragoxx10 <48144299+xxdragoxx10@users.noreply.github.com>
2019-05-06 17:49:33 -0700

    Add files via upload

--- shopping.php
+++ shopping.php
@@ -1,13 +1,14 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Shopping Page</title>
+<title>shopping Page</title>
 </head>
 <body>
-<?php
-    include("MainMenu.php");
-?>
 
+<?php
+	include("MainMenu.php");
+	
+?>
 <?php
 	include("Connect_Database.php");
 ?>
@@ -16,48 +17,71 @@
 	$selectBooks = "select * from books";
 	$results = mysqli_query($connect, $selectBooks);
 ?>
-<table align="center" border="2" width=400>
+<table class="table table-bordered table-striped table-hover">
 	<tr>
 		<th>
 			Title
 		</th>
 		<th>
-			Name
+			description
 		</th>
 		<th>
-			Post Time
+			post time
 		</th>
 		<th>
-			Book Picture
+			cost
 		</th>
+		<th>
+			picture
+		</th>
+		<th></th>
 	</tr>
 <?php
 	while($row = mysqli_fetch_assoc($results))
 	{
+		$bookTitle = $row["title"];
+		$postTime = $row["posttime"];
+		$userName = $_GET['name'];
+		$userEmail = $_GET['email'];
 		print "<tr>";
 		print "<td>";
-		print ($row["title"]);
+		print "<a href='bookreviews.php?name=$bookTitle&postime=$postTime&userName=$userName&userEmail=$userEmail'";
+		print "title=" . $row["title"] . "'>";
+		print $row["title"];
 		print "</td>";
 		print "<td>";
-		print ($row["name"]);
+		print ($row["description"]);
 		print "</td>";
 		print "<td>";
 		print ($row["posttime"]);
 		print "</td>";
-		
+		print "<td>";
+		print "$";
+		print ($row["cost"]);
+		print "</td>";
+	
 		print "<td>";
 		print "<img src='";
-		print $row["picpath"] . "' height='50' width='50'>";
+		print $row["picpath"] . "' height = 80px width = 80px>";
 		print "</td>";
-
-		// print "<td>";
-		// print "<a href='UserDelete.php?";
-		// print "email=" . $row["email"] . "'>";
-		// print "DELETE";
-		// print "</a>";
-		// print "</td>";
+		print "<td>";
+		// $book = 'bookId=' . $row["bookId"] . '&booktitle=' . $row["title"] .
+		// '&bookname=' . $row["name"] . '&picpath=' $row["picpath"];
+		print "<a href='AddCart.php?";
+		print "bookId=" . $row["bookId"] .  '&name=' . $_GET['name'] . '&email=' . $_GET['email'] . "'>";
+		print "ADD TO CART";
+		print "</td>";
 		print "</tr>";
-	}
+	
+	// 	print "<td>";
+	// 	print "<a href='UserDelete.php?";
+	// 	print "email=" . $row["email"] . "'>";
+	// 	print "DELETE";
+	// 	print "</a>";
+	// 	print "</td>";
+	// 	print "</tr>";
+	// 
+}
 ?>
 </table>
 </body>
